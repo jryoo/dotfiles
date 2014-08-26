d55f229f742c80f7d51800fcdfd0bf8bdb84df1d
Jay Ryoo <jay.ryoo@gmail.com>
2014-08-25 23:22:20 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ git pull origin master;
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "install.sh" -avh --no-perms . ~;
+    install.sh;
     source ~/.bash_profile;
 }
 
